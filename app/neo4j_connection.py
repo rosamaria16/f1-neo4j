09cc2454454cfc466e53523cddca5a3ce070a662
Rosa María Espinosa Martínez <rosamaria.espinosamartinez16@gmail.com>
2026-05-31 16:41:09 +0200

    fix: enhance neo4j_connection.py to retrieve credentials from Streamlit secrets

--- app/neo4j_connection.py
+++ app/neo4j_connection.py
@@ -1,12 +1,18 @@
 import os
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-URI = os.getenv("NEO4J_URI")
-USER = os.getenv("NEO4J_USER")
-PASSWORD = os.getenv("NEO4J_PASSWORD")
+try:
+    URI = st.secrets.get("NEO4J_URI", os.getenv("NEO4J_URI"))
+    USER = st.secrets.get("NEO4J_USER", os.getenv("NEO4J_USER"))
+    PASSWORD = st.secrets.get("NEO4J_PASSWORD", os.getenv("NEO4J_PASSWORD"))
+except:
+    URI = os.getenv("NEO4J_URI")
+    USER = os.getenv("NEO4J_USER")
+    PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
 
