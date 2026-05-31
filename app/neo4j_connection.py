ccaccb6a3ed0223b8297324199cb8c4fd2aa2afc
Rosa María Espinosa Martínez <rosamaria.espinosamartinez16@gmail.com>
2026-05-31 16:37:42 +0200

    add: create neo4j_connection.py for database connection setup

---
+++ app/neo4j_connection.py
@@ -0,0 +1,14 @@
+import os
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URI = os.getenv("NEO4J_URI")
+USER = os.getenv("NEO4J_USER")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
+
+def get_session():
+    return driver.session()
